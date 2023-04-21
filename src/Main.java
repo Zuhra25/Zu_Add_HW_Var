8a6ec58e0c8e9c89128f7384b0df52e2e2e62812
ZU <zshaihova@gmail.com>
2023-04-21 14:15:36 +0300

    задачи 6-8

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,36 @@
 public class Main {
 
     public static void main(String[] args) {
-        task1(); //task2(); task3();task4();task5();task6();task7();task8();
+        task1(); task2(); task3(); //task4();task5();task6();task7();task8();
     }
 
     public static void task1() {
-        System.out.println("Задача 1:");
-
-//Вычислите выражение a * (b + (c - d * e)) и присвойте его результат переменной *result*.
-//
-//Инвертируйте результат (значение в переменной *result*), преобразовав его в вариант
-// с противоположным значением (отрицательное -> положительное, положительное -> отрицательное).
-//
-//Выведите (напечатайте) результат в консоль.
+        System.out.println("Задача 6:");
         int a = 12;
         int b = 27;
         int c = 44;
         int d = 15;
         int e = 9;
         int result = a * (b + (c - d * e));
-        System.out.println(result);
+        System.out.println(result*-1);
+    }
+    public static void task2() {
+        System.out.println("Задача 7:");
+        int a = 5;
+        int b = 7;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + ", b = " + b);
 
+    }
+    public static void task3() {
+        System.out.println("Задача 8:");
+        int a = 754;
+        int b = a / 10;
+        b = b % 10;
 
+        System.out.println("a = " + a + ", b = " + b);
 
     }
 }
